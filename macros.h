63832f7db867e745d0ae1036745e8ee17175e799
Iago Campelo <iagow@J0T4RO-KUJ0.0>
2022-08-10 16:03:53 -0300

    cria biblioteca de macros

---
+++ macros.h
@@ -0,0 +1,12 @@
+#ifndef _MACROS_H
+#define _MACROS_H
+
+#define NOME_MAX 1000
+#define CIDADE_MAX 50
+#define PESSOA_MAX 1100 // Tamanho maximo de uma estrutura PESSOA
+#define DATA_MAX 11
+#define UF_MAX 500      // Tamanho para ler a linha do txt de UFs
+#define LOCAL_MAX 60    // Tamanho maximo de uma estrutura CIDADE-UF
+#define ERRO 1111
+
+#endif
--- main.c
+++ main.c
@@ -3,13 +3,7 @@
 #include <string.h>
 #include <ctype.h>
 #include <time.h>
-#define NOME_MAX 1000
-#define CIDADE_MAX 50
-#define PESSOA_MAX 1100 // Tamanho maximo de uma estrutura PESSOA
-#define DATA_MAX 11
-#define UF_MAX 500      // Tamanho para ler a linha do txt de UFs
-#define LOCAL_MAX 60    // Tamanho maximo de uma estrutura CIDADE-UF
-#define ERRO 1111
+#include "macros.h"
 
 typedef struct // Struct que armazena as informações de uma pessoa
 {
